7413e5cfab8ed47213fa80e1f44793809f12907c
monkypuzzle <paul.newsam@gmail.com>
2017-11-03 14:11:20 -0700

    Rounds out Section CRUD routes with Edit, Update and Destroy

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -4,10 +4,10 @@ class SectionsController < ApplicationController
     @sections = current_teacher.sections
   end
 
-  def new
-    @section = Section.new
+  def show
+    @section = Section.find(params[:id])
   end
-
+  
   def create
     @section = Section.new(section_params)
     @section.teacher_id = current_teacher.id
@@ -16,6 +16,36 @@ class SectionsController < ApplicationController
       flash[:notice] = "Section successfully created!"
     else
       render :new
+      flash[:alert] = "Something went wrong!"
+    end
+  end
+  
+  def new
+    @section = Section.new
+  end
+
+  def edit
+    @section = Section.find(params[:id])
+  end
+
+  def update
+    @section = Section.find(params[:id])
+    if @section.update(section_params)
+      redirect_to section_path(@section)
+      flash[:notice] = "Section successfully updated!"
+    else
+      redirect_to section_path(@section)
+      flash[:alert] = "Something went wrong!"
+    end
+  end
+
+  def destroy
+    @section = Section.find(params[:id])
+    if @section.destroy
+      redirect_to sections_path
+      flash[:notice] = "Section successfully deleted!"
+    else
+      redirect_to section_path(@section)
       flash[:alert] = "Something went wrong!"
     end
   end
